8a9c95d8a986e1c14fbc9c3bf10d23038e7b8341
Vishnu Narayanan V <100830902+VishnuNV007@users.noreply.github.com>
2022-08-04 21:56:25 +0530

    Add files via upload

---
+++ 4th_element_tuple.py
@@ -0,0 +1,10 @@
+#Python program to get the 4th element and 4th element from last of a tuple.
+
+tuplex = ("M","a" ,"s", "h", "u", "p", "s", "t", "a", "k")
+print(tuplex)
+#Get item (4th element)of the tuple by index
+item = tuplex[3]
+print(item)
+#Get item (4th element from last)by index negative
+item1 = tuplex[-4]
+print(item1)
